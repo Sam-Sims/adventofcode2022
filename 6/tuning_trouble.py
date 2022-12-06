7a90a54e77b8db066d902743d12e78a35bce1cca
Sam Sims <samsimss98@gmail.com>
2022-12-06 18:20:16 +0000

    tidy up

--- 6/tuning_trouble.py
+++ 6/tuning_trouble.py
@@ -5,15 +5,16 @@ def main():
     window_size = 4  # task 1
     # window_size = 14 # task 2
     pos = 0
-    # move along lines, taking 4 lines at a time
+    # sliding window
     for i in range(len(lines) - window_size + 1):
         pos += 1
         current_window = lines[i : i + window_size]
         no_repeats = len(set(current_window)) == len(current_window)
         if no_repeats:
-            print("Found a match!")
-            print(current_window)
-            print(pos + window_size - 1)
+            print(f"Found a match! The following packet detected: {current_window}")
+            print(
+                f"The packet was discovered after the {pos + window_size - 1}th packet"
+            )
             break
 
 
